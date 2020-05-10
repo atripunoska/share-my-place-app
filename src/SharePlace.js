1e1709b1fcba71da436346811e05590c2140257c
Ana <atripunoska@gmail.com>
2020-05-10 17:57:05 +0200

    Get user location

---
+++ src/SharePlace.js
@@ -0,0 +1,35 @@
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+
+    locateUserBtn.addEventListener("click", this.locateUserHandler);
+    addressForm.addEventListener("submit", this.findAddressHanlder);
+  }
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        "Location feature is not available in your browser - please use a more modern browser or manually enter an address."
+      );
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        console.log(coordinates);
+      },
+      (error) => {
+        alert(
+          "Could not locate you unfortunately. Please enter an address manually!"
+        );
+      }
+    );
+  }
+  findAddressHanlder() {}
+}
+
+new PlaceFinder();
